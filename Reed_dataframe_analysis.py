cf15f24cf05de79a02f479c8c5987fc6703ee604
Donn <donnpie@gmail.com>
2021-09-07 18:21:38 +0200

    Expanded the sample to 1000 jobs

--- Reed_dataframe_analysis.py
+++ Reed_dataframe_analysis.py
@@ -1,11 +1,11 @@
 # Analyse JSON data downloaded from reed.co.uk API and convert it to a dataframe and Excel workbook
 
 # Resources
-# https://www.youtube.com/watch?v=9N6a-VLBa2I
+# https://www.youtube.com/watch?v=9N6a-VLBa2I (Corey Schaefer video)
 # https://datatofish.com/dictionary-to-dataframe/
 # https://www.youtube.com/watch?v=ohCDWZgNIU0
 # https://www.youtube.com/watch?v=mJ3IiOjmY-M&t=321s
-# https://www.youtube.com/watch?v=pTT7HMqDnJw
+# https://www.youtube.com/watch?v=pTT7HMqDnJw (Socratica video)
 # https://datatofish.com/export-dataframe-to-excel/
 
 # Import libraries
@@ -13,7 +13,7 @@ import json
 import pandas as pd
 
 # Import data from file
-with open('json_results.txt') as f:
+with open('json_results.txt', 'r', encoding='utf-8') as f:
     data = json.load(f)
 f.close()
 #print(data)
@@ -25,19 +25,23 @@ employers = []
 locations = []
 jobTitles = []
 descriptions = []
+applications = []
 for job in data['results']:
     jobIds.append(job['jobId'])
     employers.append(job['employerName'])
     locations.append(job['locationName'])
     jobTitles.append(job['jobTitle'])
     descriptions.append(job['jobDescription'])
+    applications.append(job['applications'])
 
 new_dict['jobId'] = jobIds
 new_dict['employer'] = employers
 new_dict['location'] = locations
 new_dict['jobTitle'] = jobTitles
 new_dict['description'] = descriptions
-print(new_dict)
+new_dict['applications'] = applications
+#print(new_dict)
+print('records in dictionary: ', len(jobIds))
 
 # Convert  into dataframe
 df = pd.DataFrame(new_dict)
